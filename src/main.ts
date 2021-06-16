d686a7ad0556942134edd158f7cb28418f38fb63
NakshatraCodes <nakshatra163@gmail.com>
2021-06-16 14:33:16 +0530

    tweaks

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 4000);
-  console.log(`Application is running`);
 }
 bootstrap();
